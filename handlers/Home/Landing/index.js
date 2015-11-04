ef03d7815d54aad6346f8170bf3f605005a8fee2
Mariel Van Norman <melvinopolis@gmail.com>
2015-11-04 15:39:29 -0500

    more landing page styling, set up folders for future routes

---
+++ handlers/Home/Landing/index.js
@@ -0,0 +1,48 @@
+// // Goal is to separate out the sections between nav and footer!
+
+//           <section>
+//             <h1>Your food waste could feed Charlotte</h1>
+//             <p>Together with your food waste and our specially-designed bike trailers, we can divert large amounts of food waste from landfills.</p>
+//           </section>
+//           <section>
+//             <h2>Are we in your neighborhood?</h2>
+//             <form className="" action="#" method="post">
+//               <p>
+//                 <label for="address">Enter your street address</label>
+//                 <input type="text" name="address" id="address" value=""/>
+//               </p>
+//               <a href="in-area.html">Check address [Y]</a>
+//               <a href="out-of-area.html">Check address [N]</a>
+//               <input type="submit" value="Check address"/>
+//             </form>
+//           </section>
+//           <section>
+//             <h2>How we operate</h2>
+//             <p>You may store your compostable items in our complimentary odor-sealed four gallon bucket. On your scheduled trash collection day leave your bucket outside to be emptied, cleaned and left near your front door. Meanwhile, professional composters turn your food waste into nutrient-rich soils that you can use to grow food in your personal or community garden.</p>
+//             <p>
+//               <a href="#">See what items are compostable, recyclable and trash</a>
+//             </p>
+//           </section>
+//           <section>
+//             <h2>Composting is great for business</h2>
+//             <p>
+//               Do you work for or own a local business that is interested in learning more about the benefits of composting?
+//             </p>
+//             <p>
+//               <a href="#">Let us help your business start composting</a>
+//             </p>
+//           </section>
+//           <section>
+//             <h2>Currently serving Wilmore residents and Atherton Market guests</h2>
+//             <p>
+//               We currently operate in Charlotte’s Wilmore neighborhood.
+
+//               On Saturday mornings from 9 &ndash; 11 we host Community Compost Days at the Atherton Farmer&#39;s Market where we collect household food waste from community members whose neighborhoods we do not yet service.
+//             </p>
+//           </section>
+//           <section>
+//             <h2>We&#39;re glad you asked</h2>
+//             <p>
+//               <a href="questions.html">Find answers to popular questions</a>
+//             </p>
+//           </section>
--- handlers/Home/index.js
+++ handlers/Home/index.js
@@ -40,11 +40,13 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <div className="header">
-          <img src={require('url-loader?mimetype=image/png!../Dashboard/ctc-logo.png')} alt="Crown Town Comopost logo" className="logo"/>
+          <a href="#">
+            <img src={require('url-loader?mimetype=image/png!../Dashboard/ctc-logo.png')} alt="Crown Town Comopost logo" className="logo"/>
+          </a>
           <nav className="nav">
             <ul className="nav-tabs">
               <li><a href="#">News</a></li>
-              <li><a href="#">FAQ</a></li>
+              <li><a href="/faq">FAQ</a></li>
               {user ? accountLink : null}
               {user ? logoutLink : null}
               {!user ? signupLink : null}
@@ -54,51 +56,48 @@ class Home extends React.Component {
         </div>
 
         <main>
-          <section>
+          <section className="blue">
             <h1>Your food waste could feed Charlotte</h1>
             <p>Together with your food waste and our specially-designed bike trailers, we can divert large amounts of food waste from landfills.</p>
           </section>
-          <section>
+          <section className="grey neighborhood-check">
             <h2>Are we in your neighborhood?</h2>
             <form className="" action="#" method="post">
               <p>
-                <label for="address">Enter your street address</label>
+                <label for="address">Enter your street address:</label>
                 <input type="text" name="address" id="address" value=""/>
               </p>
-              <a href="in-area.html">Check address [Y]</a>
-              <a href="out-of-area.html">Check address [N]</a>
-              <input type="submit" value="Check address"/>
+              {/* <a href="in-area.html">Check address [Y]</a>
+              <a href="out-of-area.html">Check address [N]</a> */}
+              <input type="submit" value="Check Your Address" className="button red-button"/>
             </form>
           </section>
-          <section>
+          <section className="white">
             <h2>How we operate</h2>
-            <p>You may store your compostable items in our complimentary odor-sealed four gallon bucket. On your scheduled trash collection day leave your bucket outside to be emptied, cleaned and left near your front door. Meanwhile, professional composters turn your food waste into nutrient-rich soils that you can use to grow food in your personal or community garden.</p>
-            <p>
-              <a href="#">See what items are compostable, recyclable and trash</a>
-            </p>
+            <p>You may store your compostable items in our complimentary odor-sealed four gallon bucket.</p>
+            <p>On your scheduled trash collection day leave your bucket outside to be emptied, cleaned and left near your front door.</p> 
+            <p>Meanwhile, professional composters turn your food waste into nutrient-rich soils that you can use to grow food in your personal or community garden.</p>
+            <a href="#" className="button white-button">What can I compost?</a>
           </section>
-          <section>
+          <section className="grey">
             <h2>Composting is great for business</h2>
             <p>
               Do you work for or own a local business that is interested in learning more about the benefits of composting?
             </p>
             <p>
-              <a href="#">Let us help your business start composting</a>
+              <a href="#" className="button red-button">Let us help your business start composting</a>
             </p>
           </section>
-          <section>
+          <section className="white">
             <h2>Currently serving Wilmore residents and Atherton Market guests</h2>
-            <p>
-              We currently operate in Charlotte’s Wilmore neighborhood.
-
-              On Saturday mornings from 9 &ndash; 11 we host Community Compost Days at the Atherton Farmer&#39;s Market where we collect household food waste from community members whose neighborhoods we do not yet service.
-            </p>
+            <p>We currently operate in Charlotte’s Wilmore neighborhood.</p>
+            <p>On Saturday mornings from 9 &ndash; 11 we host Community Compost Days at the Atherton Farmer&#39;s Market where we collect household food waste from community members whose neighborhoods we do not yet service.</p>
+            <a href="#" className="button white-button">Get more info</a>
           </section>
-          <section>
-            <h2>We&#39;re glad you asked</h2>
-            <p>
-              <a href="questions.html">Find answers to popular questions</a>
-            </p>
+          <section className="grey">
+            <h2>We&#39;re glad you asked!</h2>
+            <p>There are several questions we get asked a lot. If you don&#39;t see an answer to your question, please contact us.</p>
+            <a href="faq" className="button white-button">Get answers fast</a>
           </section>
         </main>
         <footer>
@@ -120,7 +119,7 @@ class Home extends React.Component {
             </ul>
           </div>
           <div className="right">
-            <h2>Be part of our community</h2>
+            <h2>Track our Adventure</h2>
             <ul>
               <li>
                 <div className="border">
@@ -152,8 +151,8 @@ class Home extends React.Component {
       </div>
     );
   }
-  
-}
+  }
+
 
 Home.propTypes = {
   // id: React.PropTypes.any.isRequired,
